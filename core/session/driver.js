0a6c7bf8a1a041ed09e0e96aef3aa220334fda4f
PhuLuong <phult.contact@gmail.com>
2016-02-14 13:27:58 +0700

    change not implemented message

--- core/session/driver.js
+++ core/session/driver.js
@@ -7,22 +7,22 @@ module.exports = Driver;
 /** Modules **/
 function Driver(config) {
     this.get = function (sessionId, key, defaultValue) {
-        throw new Error("function is not implemented");
+        throw new Error("not implemented");
     };
     this.set = function (sessionId, key, value) {
-        throw new Error("function is not implemented");
+        throw new Error("not implemented");
     };
     this.remove = function (sessionId, key) {
-        throw new Error("function is not implemented");
+        throw new Error("not implemented");
     };
     this.pull = function (sessionId, key, defaultvalue) {
-        throw new Error("function is not implemented");
+        throw new Error("not implemented");
     };
     this.getSessions = function () {
-        throw new Error("function is not implemented");
+        throw new Error("not implemented");
     };
     this.destroy = function (sessionId) {
-        throw new Error("function is not implemented");
+        throw new Error("not implemented");
     };
 }
 
