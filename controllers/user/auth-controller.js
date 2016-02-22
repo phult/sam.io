282e8d8914b868635a023b10cde2e55562ff102e
PhuLuong <phult.contact@gmail.com>
2016-02-22 14:50:20 +0700

    Restore AuthController

---
+++ controllers/user/auth-controller.js
@@ -0,0 +1,32 @@
+module.exports = AuthController;
+function AuthController() {
+    this.namespace = "User";
+    this.login = function (io) {        
+        io = io.status(200);
+        var respondData = {};
+        // get session data
+        var user = io.session.get("user", null);
+        // user logged in
+        if (user != null) {
+            // redirect to index action
+            io.redirect("/home");
+        } else {
+            // process login
+            // ...
+            // set session data
+            io.session.set("user", io.inputs);
+            // build response data
+            respondData.status = "successful";
+            respondData.result = "login";
+        }
+        // respond
+        io.json(respondData);
+    };
+    this.logout = function (io) {
+        io.session.destroy();
+        io.json({
+            status: "successful",
+            result: "logout"
+        });
+    };    
+}
