320b9d4b2f745ee70b1949871a3c9b756b8722f9
PhuLuong <phult.contact@gmail.com>
2017-10-26 15:38:53 +0700

    set default session driver to memory

--- config/session.js
+++ config/session.js
@@ -2,7 +2,7 @@ module.exports = {
     /** Session timeout: in minutes **/
     timeout: 30,
     /** Default session driver: file, memory **/
-    driver: "file",
+    driver: "memory",
     /** Session driver storage path **/
     driverPath: "/libs/session-drivers",
     /** Session storage path **/
